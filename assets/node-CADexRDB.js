b338675fdc98f80269f8f2e0ab1639dd48b203a6
haberturdeur <haberturdeur@users.noreply.github.com>
2024-04-12 14:36:58 +0000

    deploy: f734fdf0d8a192a51de19aec00aeca342c5a0aca

---
+++ assets/node-CADexRDB.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{f as s}from"./index-DxQP2J6G.js";const{Worker:t,parentPort:o}=await s(()=>import("./__vite-browser-external-D7Ct-6yo.js").then(e=>e._),__vite__mapDeps([]));class a{worker;constructor(r){this.worker=new t(r)}postMessage(r){this.worker.postMessage(r)}onMessage(r){this.worker.on("message",r)}onError(r){this.worker.on("error",r)}async asyncDispose(){await this.worker.terminate()}async[Symbol.asyncDispose](){return this.asyncDispose()}static postMessageToParent(r){if(!o)throw new Error("Not running in a worker");o.postMessage(r)}static onMessageFromParent(r){if(!o)throw new Error("Not running in a worker");o.on("message",r)}static onErrorFromParent(r){if(!o)throw new Error("Not running in a worker");o.on("error",r)}}export{a as NodeWorker};
