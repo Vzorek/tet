1900d310d81f11d295a45d84ed2f3dd4eb96f2ae
haberturdeur <haberturdeur@users.noreply.github.com>
2024-04-18 13:52:39 +0000

    deploy: 6beaafadc9e5eb574b13851b3c8c22fa2d6f4e43

--- assets/node-KK9EKPtY.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{f as s}from"./index-B6UX0R4s.js";const{Worker:t,parentPort:o}=await s(()=>import("./__vite-browser-external-D7Ct-6yo.js").then(e=>e._),__vite__mapDeps([]),import.meta.url);class a{worker;constructor(r){this.worker=new t(r)}postMessage(r){this.worker.postMessage(r)}onMessage(r){this.worker.on("message",r)}onError(r){this.worker.on("error",r)}async asyncDispose(){await this.worker.terminate()}async[Symbol.asyncDispose](){return this.asyncDispose()}static postMessageToParent(r){if(!o)throw new Error("Not running in a worker");o.postMessage(r)}static onMessageFromParent(r){if(!o)throw new Error("Not running in a worker");o.on("message",r)}static onErrorFromParent(r){if(!o)throw new Error("Not running in a worker");o.on("error",r)}}export{a as NodeWorker};
